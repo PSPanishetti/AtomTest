a5e948661e22977a71e03d605f47dfd1a439d116
pspanishetti <praveenpanishetti@gmail.com>
2020-12-21 19:38:24 +0530

    [Design] - Login Screen UI done

---
+++ app/src/main/java/app/atom/praveen/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.atom.praveen.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.atom.praveen.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
